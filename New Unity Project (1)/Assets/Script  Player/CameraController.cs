f9f31dc12eb5b385d9620e79aafcf6ebe8770905
Wind Scar <windscaar13@gmail.com>
2019-12-05 17:50:21 +0530

    First Commit
    First Commit

---
+++ New Unity Project (1)/Assets/Script  Player/CameraController.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    #region Variables
+    public Transform target;
+    [Header("Base Settings")]
+    public float sensitivity = 2;
+    public Vector2 lookMinMax = new Vector2(-30, 30);
+    [Header("Positioning")]
+    public Vector3 offset;
+    public float transitionTime = 0.2f;
+    [Header("Field of View")]
+    public float normalViewAngle = 70;
+    public float aimViewAngle = 30;
+    public float aimAdjustmentTime = 0.05f;
+    [Header("Wall Collision")]
+    public float minDist = 0f;
+    private float maxDist;
+    public float smooth = 0.1f;
+    Vector3 dollyDir;
+    private float zDistance;
+    private float xDistance;
+    [Header("Overides")]
+    public bool overideCamera;
+
+    private float mouseX, mouseY, look, turn, finalX, finalY;
+    #endregion
+    #region On Game Start
+    private void Start()
+    {
+        dollyDir = transform.localPosition.normalized;
+        zDistance = transform.localPosition.magnitude;
+        maxDist = offset.z;
+    }
+    #endregion
+    #region Updates
+    private void Update()
+    {
+        CameraCollisionCheck();
+    }
+    void LateUpdate()
+    {
+        InputSettings();
+        Rotation();
+        OtherInputs();
+    }
+    #endregion
+    #region Completed Functions
+    void InputSettings()
+    {
+        //Look
+        mouseX -= Input.GetAxis("Mouse Y");
+        mouseX = Mathf.Clamp(mouseX, lookMinMax.x, lookMinMax.y);
+        look -= Input.GetAxis("Look");
+        look = Mathf.Clamp(look, lookMinMax.x, lookMinMax.y);
+
+        //Turn
+        mouseY += Input.GetAxis("Mouse X");
+        turn += Input.GetAxis("Turn");
+
+        finalX = (mouseX + look) * sensitivity;
+        finalY = (mouseY + turn) * sensitivity;
+    }
+    void Rotation()
+    {
+        Quaternion newRot = Quaternion.Euler(finalX, finalY, 0);
+        Quaternion playerRot = Quaternion.Euler(0, finalY, 0);
+
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0 || Input.GetButton("Aim") || overideCamera)
+            target.parent.GetComponentInParent<Transform>().rotation = Quaternion.Slerp(target.parent.GetComponent<Transform>().rotation, playerRot, transitionTime);
+
+        target.rotation = newRot;
+
+        transform.position = Vector3.Lerp(transform.position, target.transform.position - (newRot * offset), transitionTime);
+    }
+    void OtherInputs()
+    {
+        if (Input.GetButton("Aim"))
+        {
+            GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, aimViewAngle, aimAdjustmentTime);
+            Time.timeScale = 0.7f;
+        }
+        else if (GetComponent<Camera>().fieldOfView != normalViewAngle)
+        {
+            GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, normalViewAngle, aimAdjustmentTime);
+            Time.timeScale = 1f;
+        }
+    }
+    void CameraCollisionCheck()
+    {
+        Vector3 desiredCameraPos = transform.parent.TransformPoint(dollyDir * maxDist);
+        RaycastHit hit;
+
+        if (Physics.Linecast(transform.parent.position, desiredCameraPos, out hit))
+        {
+            zDistance = Mathf.Clamp((hit.distance * 0.9f), minDist, maxDist);
+        }
+        else
+        {
+            zDistance = maxDist;
+        }
+        Vector3 newPos = new Vector3(offset.x, offset.y, -zDistance);
+
+        transform.localPosition = Vector3.Lerp(newPos, dollyDir * zDistance, smooth);
+
+    }
+    #endregion
+}
---
+++ New Unity Project (1)/Assets/Script  Player/PlayerControler.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    [Header("Speed Settings")]
+    public float walkSpeed;
+    public float runSpeed;
+    private float speed;
+    [Header("Jump Settings")]
+    public float jumpForce;
+    public ForceMode forceType;
+
+    bool isFalling;
+    [Header("Animatios")]
+    public Animator anim;//Animator
+    public float transitionTime;//Time to move to new pose
+    float curLightAttack; // Current Attack Phase
+
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent < Rigidbody>();
+    }
+
+    private void Update()
+    {
+        #region Inputs
+        //Sprint
+        if (Input.GetButton("Sprint"))
+            speed = runSpeed;
+        else if (speed != walkSpeed)
+            speed = walkSpeed;
+        
+        
+        //Jump
+        if (Input.GetButton("Jump") && !isFalling)
+            Jump(jumpForce, forceType);
+
+        #endregion
+
+        #region Animations
+        if (speed == runSpeed)
+        {
+            AxisAnimationUpdate("Horizontal", "Direction", 1);
+            AxisAnimationUpdate("Vertical", "Speed", 1);
+        }
+        else
+        {
+            AxisAnimationUpdate("Horizontal", "Direction", 0.5f);
+            AxisAnimationUpdate("Vertical", "Speed", 0.5f);
+        }
+
+        anim.SetBool("isFalling", isFalling);
+        
+        if (!anim.GetCurrentAnimatorStateInfo(1).IsName("Default"))
+        {
+            FindObjectOfType<CameraController>().overideCamera = true;
+        }
+        else
+        {
+            FindObjectOfType<CameraController>().overideCamera = false;
+        }
+
+      if (Input.GetButtonDown("Fire1")&& anim.GetCurrentAnimatorStateInfo(1).IsName("Default"))
+        {
+           
+             Attacking();
+           
+        }
+        #endregion
+    }
+
+
+    #region
+
+    private void Attacking()
+    {
+        anim.SetTrigger("Attack");
+    }
+
+    #endregion
+
+    private void FixedUpdate()
+    {
+
+        #region Motion
+        var x = Input.GetAxis("Horizontal");
+        var z = Input.GetAxis("Vertical");
+        rb.MovePosition(transform.position + transform.TransformDirection(x, 0, z) * Time.deltaTime * speed);
+        #endregion
+    }
+
+    #region Completed Function
+
+    void Jump(float force,ForceMode type)
+    {
+        isFalling = true;
+        rb.velocity = Vector3.zero;
+        rb.AddForce(transform.up * force, type);
+    }
+
+    void AxisAnimationUpdate(string AxisName,string motionName, float motionSpeed)
+    {
+        if (Input.GetAxis(AxisName) > 0)
+            anim.SetFloat(motionName, Mathf.Lerp(anim.GetFloat(motionName), motionSpeed, transitionTime * Time.deltaTime));
+        else if (Input.GetAxis(AxisName) < 0)
+            anim.SetFloat(motionName, Mathf.Lerp(anim.GetFloat(motionName), -motionSpeed, transitionTime * Time.deltaTime));
+        else
+            anim.SetFloat(motionName, Mathf.Lerp(anim.GetFloat(motionName), 0, transitionTime * Time.deltaTime));
+
+
+
+
+    }
+
+
+    #endregion
+
+    #region Collisions And Triggers
+    private void OnTriggerStay(Collider other)
+    {
+        
+        if (isFalling && other.tag!="Player")
+            isFalling = false;
+    }
+    #endregion
+
+}
